ad8d1e991b6bd1aa28706d627a593958bdb34046
Hatim Malak <hatim05042006@gmail.com>
2026-01-03 21:02:02 +0530

    project completed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <div className="w-full lg:h-[20%] h-[30%] flex lg:flex-row flex-col-reverse justify-between items-center lg:p-9 p-5 bg-gray-400/15 backdrop-blur-md border-t-2 border-gray-400/30 ">
       <div className="text-white font-medium">
-        <h1>&copy; Hatim Built with MERN</h1>
+        <h1>&copy; Built with MERN</h1>
       </div>
       <div className="flex justify-center items-center text-white font-bold gap-10">
         <Link >Home</Link>
